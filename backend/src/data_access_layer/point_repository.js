fd5cc9825c600f36043688902163a73c1cef264f
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-04-05 11:59:38 +0200

    creted a new function

--- backend/src/data_access_layer/point_repository.js
+++ backend/src/data_access_layer/point_repository.js
@@ -15,6 +15,16 @@ module.exports = function (){
      this.exports.getPointById = function(id, callback){
  
      }
+
+     /**
+      * @param {number} x 
+      * @param {number} y
+      * @param {Date} dateTime
+      * @param {Map<String, Any>} callback
+     */
+     this.exports.addPoint = function(x ,y ,dateTime, callback){
+          
+     }
  
      return this.exports
 
