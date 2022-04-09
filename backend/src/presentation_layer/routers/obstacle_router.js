dfa0e6f0b6cbc3660b50901672a08faf8f20a50b
tifufu <dematasjoshua@hotmail.com>
2022-04-09 20:24:58 +0200

    Added data and data/tmp to gitignore

--- backend/src/presentation_layer/routers/obstacle_router.js
+++ backend/src/presentation_layer/routers/obstacle_router.js
@@ -1,20 +1,19 @@
 const express = require("express")
 const multer = require("multer")
+const path = require("path")
 
 module.exports = () => {
   const router = express.Router()
   const multerStorageConfig = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "../../data_access_layer/data/tmp/")
-    },
-    filename: (req, file, cb) => {
-      const fileName = file.originalname.toLowerCase().split(" ").join("-")
-      cb(null, fileName)
+      cb(null, path.join(__dirname, "./../../data_access_layer/data/tmp/"))
     }
   })
-  const multerUploadMiddleware = multer({ storage: storage })
+  const multerUpload = multer({ storage: multerStorageConfig })
 
-  router.put('/event', multerUploadMiddleware, (req, res, next) => {
+  router.put('/event', multerUpload.single('image'), (req, res, next) => {
     res.status(201).send('OK');
   })
+
+  return router
 }
