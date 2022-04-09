0fd8bbffc628556df47fa3e6cbcdeec431a29c89
tifufu <dematasjoshua@hotmail.com>
2022-04-09 19:54:36 +0200

    Added the obstacle router to awilix container and the express app

--- backend/src/presentation_layer/app.js
+++ backend/src/presentation_layer/app.js
@@ -12,6 +12,7 @@ const routeRouter = require('./routers/router')
 const pointRepository = require('../data_access_layer/point_repository')
 const pointManager = require('../business_logic_layer/point_manager')
 const pointRouter = require('./routers/point_router')
+const obstacleRouter = require('./routers/obstacle_router')
 
 // awilix containers
 const container = awilix.createContainer()
@@ -23,17 +24,20 @@ container.register('routeRouter', awilix.asFunction(routeRouter))
 container.register('pointRepository', awilix.asFunction(pointRepository))
 container.register('pointManager', awilix.asFunction(pointManager))
 container.register('pointRouter', awilix.asFunction(pointRouter))
+container.register('obstacleRouter', awilix.asFunction(obstacleRouter))
 
 //container.register('db', awilix.asFunction(db))
 // resolve the containers
 
 const route_router = container.resolve('routeRouter')
 const point_router = container.resolve('pointRouter')
+const obstacle_router = container.resolve('obstacleRouter')
 
 // use the routes
 
 app.use('/', route_router)
 app.use('/point', point_router)
+app.use('/obstacle', obstacle_router)
 
 app.listen(8080, function () {
   console.log("Web application listening on port 3000.")
--- backend/src/presentation_layer/routers/obstacle_router.js
+++ backend/src/presentation_layer/routers/obstacle_router.js
@@ -1,8 +1,20 @@
-const express = require("express");
-module.exports = () => {
-  const router = express.Router();
+const express = require("express")
+const multer = require("multer")
 
-  router.put('/event', (req, res, next) => {
-    
-  });
-};
+module.exports = () => {
+  const router = express.Router()
+  const multerStorageConfig = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "../../data_access_layer/data/tmp/")
+    },
+    filename: (req, file, cb) => {
+      const fileName = file.originalname.toLowerCase().split(" ").join("-")
+      cb(null, fileName)
+    }
+  })
+  const multerUploadMiddleware = multer({ storage: storage })
+
+  router.put('/event', multerUploadMiddleware, (req, res, next) => {
+    res.status(201).send('OK');
+  })
+}
