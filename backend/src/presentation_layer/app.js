cf8fc5380689c5f516456e84570fb1891d8a21c3
Abdel Rahman Mallah <55826317+AbdoMallah@users.noreply.github.com>
2022-04-21 15:33:07 +0200

    Revert "Removed the Border Files and Router Files "

--- backend/src/presentation_layer/app.js
+++ backend/src/presentation_layer/app.js
@@ -8,6 +8,10 @@ require('../data_access_layer/database')
 
 
 //reposositories (replace the code below to our needs)
+const routerRepository = require('../data_access_layer/router_repository')
+const routerManager = require('../business_logic_layer/router_manager')
+const routeRouter = require('./routers/router')
+
 const pointRepository = require('../data_access_layer/point_repository')
 const pointManager = require('../business_logic_layer/point_manager')
 const pointRouter = require('./routers/point_router')
@@ -16,14 +20,24 @@ const mapRepository = require('../data_access_layer/map_repository')
 const mapManager = require('../business_logic_layer/map_manager')
 const mapRouter = require('./routers/map_router')
 
+const borderRepository = require('../data_access_layer/border_repository')
+const borderManager = require('../business_logic_layer/border_manager')
+const borderRouter = require('./routers/border_router')
+
 // awilix containers
 const container = awilix.createContainer()
 
+container.register('routerRepository', awilix.asFunction(routerRepository))
+container.register('routerManager', awilix.asFunction(routerManager))
+container.register('routeRouter', awilix.asFunction(routeRouter))
 
 container.register('pointRepository', awilix.asFunction(pointRepository))
 container.register('pointManager', awilix.asFunction(pointManager))
 container.register('pointRouter', awilix.asFunction(pointRouter))
 
+container.register('borderRepository', awilix.asFunction(borderRepository))
+container.register('borderManager', awilix.asFunction(borderManager))
+container.register('borderRouter', awilix.asFunction(borderRouter))
 
 container.register('mapRepository', awilix.asFunction(mapRepository))
 container.register('mapManager', awilix.asFunction(mapManager))
