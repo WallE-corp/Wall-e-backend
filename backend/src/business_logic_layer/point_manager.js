49f04f0770e375ad8d86e23b860e4ba117322ad6
AbdoMallah <55826317+AbdoMallah@users.noreply.github.com>
2022-04-26 12:48:34 +0200

    pointManager
    implemented managerPoint function in point_manager. that takes a point and update it. the new updatedPoint should be added to the database. not the original one.

--- backend/src/business_logic_layer/point_manager.js
+++ backend/src/business_logic_layer/point_manager.js
@@ -1,13 +1,37 @@
-module.exports = function ({ pointRepository }){
-     return {
-              
-       getAllPathPoints: function (callback) {
+module.exports = function ({ pointRepository }) {
+    return {
+
+        getAllPathPoints: function (callback) {
             pointRepository.getAllPathPoints(callback)
-        },       
-       
-       addPoint: function( coordinates, callback) {
-            if( !coordinates.x || !coordinates.y){ return }
-            pointRepository.addPoint( coordinates ,callback)
+        },
+
+
+        managePoint: function (point, callback) {
+            if (Object.keys(point).empty) {
+                callback('pointEmpty', null)
+            } else {
+                pointRepository.getAllPathPoints((error, points) => {
+                    if (error) {
+                        callback(error, null)
+                    }
+                    else {
+                        // Get last Point
+                        let lastPoint = points[points.length - 1]
+                        // Update x value in the point
+                        point.x = point.x + lastPoint.x
+                        // Update x value in the point
+                        point.y = point.y + lastPoint.y
+                        // Callback the updated point
+                        callback(null, point)
+
+                    }
+                })
+
+            }
+        },
+        addPoint: function (coordinates, callback) {
+            if (!coordinates.x || !coordinates.y) { return }
+            pointRepository.addPoint(coordinates, callback)
         },
 
         /**
@@ -15,15 +39,15 @@ module.exports = function ({ pointRepository }){
          * @param {String} mapId 
          * @param {void} callback 
          */
-        getPointByCoordinateManager: function(mapId,coordinates, callback){
-            if(mapId.length == 0) callback('InvalidMapId',null)
-            else if(( coordinates.x && coordinates.y )){
-                pointRepository.getPointByCoordinate(mapId,coordinates,callback)
-            }else{
-                callback('InvalidCoordinates',null)
+        getPointByCoordinateManager: function (mapId, coordinates, callback) {
+            if (mapId.length == 0) callback('InvalidMapId', null)
+            else if ((coordinates.x && coordinates.y)) {
+                pointRepository.getPointByCoordinate(mapId, coordinates, callback)
+            } else {
+                callback('InvalidCoordinates', null)
             }
         }
 
-     }
- 
- }
+    }
+
+}
--- backend/src/presentation_layer/routers/point_router.js
+++ backend/src/presentation_layer/routers/point_router.js
@@ -2,50 +2,50 @@ const { response } = require('express')
 const express = require('express')
 
 
-module.exports = function ({pointManager}){
+module.exports = function ({ pointManager }) {
 
     const router = express.Router()
-    
-     router.get("/", (req, res) => {
+
+    router.get("/", (req, res) => {
         pointManager.getAllPathPoints((error, data) => {
-            if (error){
+            if (error) {
                 res.status(500).json(error)
             }
-            else{
+            else {
                 res.status(200).json(data)
             }
         })
     })
-  
+
     router.get("/:mapId", (req, res) => {
         const mapId = req.params.mapId;
-        
-        const coordinates = { 
-            x : parseFloat(req.body.x),
-            y : parseFloat(req.body.y)
-        }
-        
 
-        pointManager.getPointByCoordinateManager(mapId, coordinates, function(error, point){
-            if(error){
+        const coordinates = {
+            x: parseFloat(req.body.x),
+            y: parseFloat(req.body.y)
+        }
+
+
+        pointManager.getPointByCoordinateManager(mapId, coordinates, function (error, point) {
+            if (error) {
                 res.status(500).json(error)
-            }else{
+            } else {
                 res.status(200).json(point)
             }
         })
     })
-    
+
     router.post('/', (req, res) => {
         const x = req.body.x
         const y = req.body.y
 
-        pointManager.addPoint({x, y}, function(errors) {
-            if(errors){ return res.status(500)}
+        pointManager.addPoint({ x, y }, function (errors) {
+            if (errors) { return res.status(500) }
             res.status(201)
             console.log("status ok")
         })
     })
 
-   return router
-   
+    return router
+
 }
