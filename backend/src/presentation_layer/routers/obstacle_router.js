d643b3e67159bcc2f06f8c49e3e6e7916dd3ef73
tifufu <dematasjoshua@hotmail.com>
2022-04-30 05:05:32 +0200

    Fixed coding style in obstacle_router

--- backend/src/presentation_layer/routers/obstacle_router.js
+++ backend/src/presentation_layer/routers/obstacle_router.js
@@ -3,22 +3,22 @@ const multer = require("multer")
 const path = require("path")
 
 module.exports = ({ obstacleEventManager }) => {
-  const router = express.Router()
-  const multerStorageConfig = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, "./../../data_access_layer/data/tmp/"))
-    }
-  })
-  const multerUpload = multer({ storage: multerStorageConfig })
+    const router = express.Router()
+    const multerStorageConfig = multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, path.join(__dirname, "./../../data_access_layer/data/tmp/"))
+        }
+    })
+    const multerUpload = multer({ storage: multerStorageConfig })
 
-  router.put('/event', multerUpload.single('image'), (req, res, next) => {
-    const obstacle_event_data = {
-      ...req.body,
-      tmpImageFilePath: req.file.path
-    }
-    obstacleEventManager.handle_obstacle_event(obstacle_event_data)
-    res.status(201).send('OK');
-  })
+    router.put('/event', multerUpload.single('image'), (req, res, next) => {
+        const obstacleEventData = {
+            ...req.body,
+            tmpImageFilePath: req.file.path
+        }
+        obstacleEventManager.handle_obstacle_event(obstacleEventData)
+        res.status(201).send('OK')
+    })
 
-  return router
+    return router
 }
