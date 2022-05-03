b426142a411d6aab12bc777918c2b475c0b25efe
tifufu <dematasjoshua@hotmail.com>
2022-05-03 08:26:07 +0200

    uploadImage test now uses storage emulator

--- backend/testing/upload_image.test.js
+++ backend/testing/upload_image.test.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 const uploadImage = require('../src/business_logic_layer/utility/upload_image')
-const { Storage } = require('@google-cloud/storage')
+const firebaseAdmin = require("firebase-admin")
 const path = require('path')
 
 describe('Upload Image to Cloud', () => {
     let _uploadImage
-    let storage
+
     beforeAll(() => {
-        storage = new Storage()
-        _uploadImage = uploadImage({ storage })
+        firebaseAdmin.initializeApp({
+            projectId: process.env.FIREBASE_PROJECT_ID
+        })
+        _uploadImage = uploadImage({ storage: firebaseAdmin.storage() })
     })
 
     it('Should upload image and return its destination', async () => {
