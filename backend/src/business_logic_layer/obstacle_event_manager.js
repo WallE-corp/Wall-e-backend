1b26fb9cc00c35751b846f0e859e74b97a3596cc
tifufu <dematasjoshua@hotmail.com>
2022-05-05 04:27:40 +0200

    Moved the code to push obstacle event to mobile into its own function

--- backend/src/business_logic_layer/obstacle_event_manager.js
+++ backend/src/business_logic_layer/obstacle_event_manager.js
@@ -2,41 +2,46 @@
 const { OBSTACLE_EVENT } = require('../presentation_layer/socketio/command_types')
 
 module.exports = ({ obstacleEventRepository, uploadImage, SocketIOServer, dtoValidator, clearTmpFile }) => {
-    return {
-        handleObstacleEvent: async (obstacleEventData) => {
-            obstacleEventData.x = Number(obstacleEventData.x)
-            obstacleEventData.y = Number(obstacleEventData.y)
-            const validationResult = dtoValidator.validateObstacleEventDto(obstacleEventData)
-            if (!validationResult) {
-                clearTmpFile(obstacleEventData.tmpImageFilePath)
-                throw "Validation Error"
-            }
+    async function pushObstacleEvent (obstacleEvent) {
+        SocketIOServer.sendCommand(OBSTACLE_EVENT, null, obstacleEvent)
+    }
 
-            // TODO: Correct x and y values to get reall position on map
-            const { tmpImageFilePath, x, y } = obstacleEventData
-
-            try {
-                // Store image in Cloud Storage
-                const imageUrl = await uploadImage(tmpImageFilePath)
-
-                // Clear tmp file
-                clearTmpFile(tmpImageFilePath)
-
-                // Begin async request to classify image
-                const classification = await obstacleEventRepository.getImageClassification(imageUrl)
-
-                // Create an obstacle event document in Cloud Firestore
-                const obstacleEvent = await obstacleEventRepository.addObstacleEvent(imageUrl, x, y, classification)
-
-                // notify mobile of obstacle event
-                SocketIOServer.sendCommand(OBSTACLE_EVENT, null, obstacleEvent)
-
-                return obstacleEvent
-            } catch (e) {
-                console.error(e)
-                throw e
-            }
+    async function handleObstacleEvent (obstacleEventData) {
+        obstacleEventData.x = Number(obstacleEventData.x)
+        obstacleEventData.y = Number(obstacleEventData.y)
+        const validationResult = dtoValidator.validateObstacleEventDto(obstacleEventData)
+        if (!validationResult) {
+            clearTmpFile(obstacleEventData.tmpImageFilePath)
+            throw "Validation Error"
         }
 
+        // TODO: Correct x and y values to get reall position on map
+        const { tmpImageFilePath, x, y } = obstacleEventData
+
+        try {
+            // Store image in Cloud Storage
+            const imageUrl = await uploadImage(tmpImageFilePath)
+
+            // Clear tmp file
+            clearTmpFile(tmpImageFilePath)
+
+            // Begin async request to classify image
+            const classification = await obstacleEventRepository.getImageClassification(imageUrl)
+
+            // Create an obstacle event document in Cloud Firestore
+            const obstacleEvent = await obstacleEventRepository.addObstacleEvent(imageUrl, x, y, classification)
+
+            // notify mobile of obstacle event
+            pushObstacleEvent(obstacleEvent)
+
+            return obstacleEvent
+        } catch (e) {
+            console.error(e)
+            throw e
+        }
+    }
+
+    return {
+        handleObstacleEvent
     }
 }
