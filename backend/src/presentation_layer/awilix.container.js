fefd9fc11978bdb633b666f2c2fbad252782bd01
Ahmad adnan Abdullah <70108548+abah19vo@users.noreply.github.com>
2022-05-17 13:53:51 +0200

    Still working on the router
    Co-Authored-By: Abdel Rahman Mallah <55826317+AbdoMallah@users.noreply.github.com>

--- backend/src/presentation_layer/awilix.container.js
+++ backend/src/presentation_layer/awilix.container.js
@@ -4,14 +4,9 @@ const awilix = require('awilix')
 const container = awilix.createContainer()
 
 // Get db connection
-const { getDatabaseConnection, getStorageConnection, getAdminSDK } = require('../data_access_layer/database')
-const db = getDatabaseConnection()
-const storage = getStorageConnection()
-const admin = getAdminSDK()
-container.register('db',awilix.asValue(db))
-container.register('storage',awilix.asValue(storage))
-container.register('admin',awilix.asValue(admin))
-
+const db = require('../data_access_layer/database')
+const db = getDbConnection()
+container.register('db', awilix.asValue(db))
 
 // reposositories (replace the code below to our needs)
 const pointRepository = require('../data_access_layer/point_repository')
@@ -24,7 +19,6 @@ const mapRouter = require('./routers/map_router')
 
 const obstacleRouter = require('./routers/obstacle_router')
 const obstacleEventManager = require('../business_logic_layer/obstacle_event_manager')
-const obstacleEventRepository = require('../data_access_layer/obstacle_event_repository')
 
 const SocketIOServer = require('./socketio/')
 
@@ -36,16 +30,17 @@ container.register('mapRepository', awilix.asFunction(mapRepository))
 container.register('mapManager', awilix.asFunction(mapManager))
 container.register('mapRouter', awilix.asFunction(mapRouter))
 
-container.register('SocketIOServer', awilix.asClass(SocketIOServer).setLifetime(awilix.Lifetime.SINGLETON))
+container.register('SocketIOServer', awilix.asClass(SocketIOServer))
 container.register('obstacleRouter', awilix.asFunction(obstacleRouter))
 container.register('obstacleEventManager', awilix.asFunction(obstacleEventManager))
-container.register('obstacleEventRepository', awilix.asFunction(obstacleEventRepository))
 
 const { Storage } = require('@google-cloud/storage')
 container.register('storage', awilix.asClass(Storage))
 
 const uploadImage = require('../business_logic_layer/utility/upload_image')
 container.register('uploadImage', awilix.asFunction(uploadImage))
+
+module.exports = container
 
 // const modules = [
 //     'src/data_access_layer/**/*.js',
@@ -58,5 +53,3 @@ container.register('uploadImage', awilix.asFunction(uploadImage))
 
 // const autoLoadedModules = awilix.listModules(modules)
 // console.log("Auto-loaded modules", autoLoadedModules.map(m => m.name))
-
-module.exports = container
